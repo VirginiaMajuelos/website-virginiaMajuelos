c39c8414c5e9ed175353db81ef7f31143085dc1b
Virginia Majuelos <virginia.majuelos@gmail.com>
2025-04-26 19:39:28 +0200

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://VirginiaMajuelos.github.io",
+  site: "https://virginiamajuelos.github.io/website-virginiaMajuelos/",
   base: "/website-virginiaMajuelos",
 });
